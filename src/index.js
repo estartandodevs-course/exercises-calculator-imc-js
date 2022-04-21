90c6294a23e66fdac2f4613964c37d60e8b84be7
Felipe Rosas <contato@eufelipe.com>
2022-04-21 09:50:55 -0300

    feat: add initial files

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// TODO 1:
+// Implemente a função calculateImc para que ela receba os valores
+// De Peso e Altura e retorne o valor do IMC e a descrição do IMC .
+
+// Exemplo de retorno:  { imc: 23, description: "Sobrepeso" }
+// Você pode usar o array de opções imcResultOptions.js para descrever o retorno.
+
+// TODO 2:
+// Retornar um erro se não receber os valores de peso e altura.
+
+// TODO 3:
+// Retornar um erro se os valores de peso ou altura forem negativos.
+
+// TODO 4:
+// Retornar um erro se os valores de peso ou altura não forem números
+
+function calculateImc(weight, height) {
+  // TODO: Implemente aqui
+}
+
+module.exports = calculateImc;
