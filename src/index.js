6ac49515d3c99cfce84810231f8f5a8b578e4434
jonataspinto <jonatastdb_jp@hotmail.com>
2022-05-01 01:41:36 -0300

    feat: test evaluator

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 // Implemente a função calculateImc para que ela receba os valores
 // de Peso e Altura e retorne o valor do IMC e a descrição do IMC .
 
+const options = require("./imcResultOptions");
+
 // Exemplo de retorno:  { imc: 23, description: "Sobrepeso" }
 // Você pode usar o array de opções imcResultOptions.js para descrever o retorno.
 
@@ -15,6 +17,17 @@
 // Retornar o erro:  "weight and/or height are not numbers" se os valores de peso ou altura não forem números.
 
 function calculateImc(weight, height) {
+  const IMC = weight / ((height/100)*(height/100));
+  const result  = options.find(option => {
+    if(IMC >= option.min && IMC <= option.max) {
+      return option
+    }
+  })
+
+  return {
+    imc: Number.parseInt(IMC, 10),
+    description: result?.description
+  }
   // TODO: Implemente aqui
 }
 
