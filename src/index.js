5970705d46d1db926ff15d2bc87c18d324bb4e3d
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-05-31 00:41:54 -0300

    ex-resolved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const options = require('./imcResultOptions');
+
 // TODO 1:
 // Implemente a função calculateImc para que ela receba os valores
 // de Peso e Altura e retorne o valor do IMC e a descrição do IMC .
@@ -16,6 +18,44 @@
 
 function calculateImc(weight, height) {
   // TODO: Implemente aqui
+
+
+  let imc = Math.floor(((weight / (height/100 * height/100))));
+  let desc = '';
+
+
+   const getDescription = (options) => {
+     options.forEach((el) => {
+      const { min, max, description } = el
+      if (imc >= min && imc <= max){
+        desc =  description;
+      }
+    })
+  }
+  getDescription(options)
+
+  const isParamsValid = (weight, height) => {
+    if (weight == undefined && height == undefined){
+      throw new Error('weight and height are required')
+    } else if (height == undefined){
+    throw new Error('weight and height are required')
+    }
+
+    if ( weight < 0 || height < 0 ){
+    throw new Error('weight and height cannot be negative')
+    } else if ( isNaN(weight) || isNaN(height)){
+    throw new Error('weight and/or height are not numbers')
+    }
+  }
+
+  isParamsValid(weight,height)
+
+  return {
+    imc : imc,
+    description: desc
+  }
 }
+
+
 
 module.exports = calculateImc;
